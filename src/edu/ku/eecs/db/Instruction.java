d7aaa88e06d61842ec48c8fcc2cdc07da614000c
Howard Grimberg <hgrimberg@ku.edu>
2013-12-01 13:39:11 -0600

    Build

---
+++ src/edu/ku/eecs/db/Instruction.java
@@ -0,0 +1,41 @@
+package edu.ku.eecs.db;
+/**
+ * 
+ * @author hgrimberg
+ *
+ */
+public class Instruction {
+	public static enum Type {
+		INSERT, DELETE, SEARCH, OUTPUT
+	}
+
+	private Type iType;
+
+	public Type getiType() {
+		return iType;
+	}
+
+	public void setiType(Type iType) {
+		this.iType = iType;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public void setVal(String val) {
+		this.val = val;
+	}
+
+	private String key;
+	private String val;
+
+}
